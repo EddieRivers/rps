a79878c5a798f17e25d2e19a48a335642a2f7dd6
EddieRivers <eduardogallego2020@gmail.com>
2024-10-20 20:03:45 +0200

    Updated service GameService

--- rps/src/main/java/com/example/rps/service/GameService.java
+++ rps/src/main/java/com/example/rps/service/GameService.java
@@ -1,17 +1,37 @@
 package com.example.rps.service;
 
+import com.example.rps.model.GlobalStats;
 import com.example.rps.model.Move;
 import com.example.rps.model.RoundResult;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GameService {
+
+    @Autowired
+    private GlobalStats globalStats;
 
     public RoundResult playRound() {
         Move player1Move = Move.getRandomMove();
         Move player2Move = Move.ROCK;
 
         String result = determineWinner(player1Move, player2Move);
+
+        // Update Global Statistics
+        globalStats.incrementTotalRoundsPlayed();
+        switch (result) {
+            case "Draw":
+                globalStats.incrementTotalDraws();
+                break;
+            case "Player 1 Wins":
+                globalStats.incrementTotalWinsPlayer1();
+                break;
+            case "Player 2 Wins":
+                globalStats.incrementTotalWinsPlayer2();
+                break;
+        }
 
         return new RoundResult(player1Move, player2Move, result);
     }
